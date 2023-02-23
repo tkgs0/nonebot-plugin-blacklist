05182658954d614884e8a29f4f0bf45b449682bd
tkgs0 <hgc.sakuya@ya.ru>
2023-02-23 14:36:10 +0800

    :art:

--- nonebot_plugin_blacklist/__init__.py
+++ nonebot_plugin_blacklist/__init__.py
@@ -222,7 +222,7 @@ reset_blacklist = on_command('重置黑名单', aliases={'清空黑名单'}, per
 
 @reset_blacklist.got('flag', prompt='确定重置黑名单? (Y/n)')
 async def reset_list(flag: str = ArgStr('flag')):
-    if flag in ['Y', 'Yes', 'True']:
+    if flag.lower().strip() in ['y', 'yes', 'true']:
         blacklist['userlist'].clear()
         blacklist['grouplist'].clear()
         save_blacklist()
